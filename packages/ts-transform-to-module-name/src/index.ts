0b63d6d6c8a3f47ffcf3aee2a6ec770e78ec236f
Tony Hallett <tonyhallett74@gmail.com>
2026-01-29 14:53:14 +0000

    isolated modules

--- packages/ts-transform-to-module-name/src/index.ts
+++ packages/ts-transform-to-module-name/src/index.ts
@@ -1,4 +1,5 @@
 import { transformToModuleName } from "./transformToModuleName";
 import { transformToModuleNameFactory } from "./transformToModuleNameFactory";
 import { AdditionalTransformFactory } from "./AdditionalTransformFactory";
-export { AdditionalTransformFactory, transformToModuleNameFactory, transformToModuleName };
+export { transformToModuleNameFactory, transformToModuleName };
+export type { AdditionalTransformFactory };
