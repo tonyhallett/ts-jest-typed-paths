f7d4caf434a999f1905b991121b55c6bb09da403
Tony Hallett <tonyhallett74@gmail.com>
2026-02-08 11:49:58 +0000

    add jsdoc and error info to transformToModuleName

--- packages/ts-transform-to-module-name/src/transformToModuleName.ts
+++ packages/ts-transform-to-module-name/src/transformToModuleName.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // marker function
 /* istanbul ignore next */
+/**
+ * This function is a marker function.  Invoke in code with a generic type that is imported. The transfomer will remove the call expression and replace with the imported module name.
+ */
 export default function transformToModuleName<T>(): string {
-  throw new Error("");
+  throw new Error(
+    "transformToModuleName should only be used as a marker function and should have been removed by the transformer",
+  );
 }
